a11ecd5984e0dc0904d667a154c1da27c660b632
Arputhan-13 <karputhan@gmail.com>
2026-06-08 22:27:19 +0530

    Create C_problem_9.c

---
+++ C_problem_9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b, c=0;
+    a=10;
+    while(a<100)
+    {
+    b=a%10;
+    if(b==5)
+    {
+    c=c+a;
+    }
+    a++;
+    }
+    printf("%d\n",c);
+    
+    
+}
